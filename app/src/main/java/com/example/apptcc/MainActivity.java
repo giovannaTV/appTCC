02ec84b2a9c0735c8f54ad0d938b2eb9875c6f3a
Giovanna Taglialegna Verola Moreira <giovanna.verola2016@gmail.com>
2021-05-31 22:04:27 -0300

    Tela para acessar os jogos.

--- app/src/main/java/com/example/apptcc/MainActivity.java
+++ app/src/main/java/com/example/apptcc/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.apptcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botaoIrParaTelaJogos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoIrParaTelaJogos = (Button) findViewById(R.id.buttonJogos);
+
+        botaoIrParaTelaJogos.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), telaJogos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
